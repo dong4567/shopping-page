63cf33344a53414b5d314128d5e874ed57fbd276
dong4567 <minhdongpaham@gmail.com>
2023-10-14 21:04:12 +0700

    Init project

---
+++ assets/js/app.js
@@ -0,0 +1,17 @@
+const nameInp = document.getElementById('name');
+const date = document.getElementById('date');
+const phone = document.getElementById('phone');
+const Email = document.getElementById('Email');
+const gender = document.getElementById('gender');
+const password = document.getElementById('password');
+const repeatPassword = document.getElementById('repeat-password');
+const signupBtn = document.getElementById('btn');
+const signupform = document.getElementById('sign-up-form');
+
+function signuphandle(event){
+    event.preventDefault();
+    
+    if() {
+
+    }
+}
